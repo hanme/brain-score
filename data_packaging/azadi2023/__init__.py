fd44770fcf0b9df5c79639a8230f41b023bb249f
hanme <johannes.mehrer@epfl.ch>
2023-08-21 09:34:57 +0200

    added remaining assembly collections

--- data_packaging/azadi2023/__init__.py
+++ data_packaging/azadi2023/__init__.py
@@ -39,16 +39,12 @@ def collect_stimuli():
     return stimulus_set
 
 
-#def collect_assembly():
-#    XXX
-
-
 def collect_stimulation_report_rate_training():
     """ fig 1C (only data for monkey Ph given) """
     # data extracted with https://apps.automeris.io/wpd/ on 2023-08-18, points manually selected
     data = pd.read_csv(Path(__file__).parent / 'Azadi2023_fig1C.csv')
 
-     # package into xarray
+    # package into xarray
     assembly = DataAssembly(data['stimulation_report_rate'], coords={
         'monkey': ('measurement', data['monkey']),
         'session_number': ('measurement', data['session_number']),
@@ -66,3 +62,86 @@ def collect_stimulation_report_rate_training():
     assembly = DataAssembly(assembly)
     return assembly
 
+
+def collect_detection_profile():
+    """ fig2A & figS2 """
+    # data extracted with https://apps.automeris.io/wpd/ on 2023-08-18, points manually selected
+    data = pd.read_csv(Path(__file__).parent / 'Azadi2023_fig2A_figS2.csv')
+    
+    # package into xarray
+    assembly = DataAssembly(data['performance_d_prime'], coords={
+        'monkey': ('measurement', data['monkey']),
+        'site': ('measurement', data['site']),
+        'image_idx': ('measurement', data['image_idx']),
+        'summary_stats': ('measurement', data['summary_stats']),
+    }, dims=['measurement'])
+    assembly = assembly.unstack()
+    assembly['monkey_Ph'] = ('monkey', [monkey == 'Ph' for monkey in assembly['monkey']])
+    assembly['monkey_Sp'] = ('monkey', [monkey == 'Sp' for monkey in assembly['monkey']])
+    assembly = assembly.stack(condition=['monkey'])
+    assembly = DataAssembly(assembly)
+    return assembly
+
+
+def collect_corr_between_detection_profiles():
+    """ Azadi2023_fig2B_fig_S3A """
+    # data extracted with https://apps.automeris.io/wpd/ on 2023-08-18, points manually selected
+    data = pd.read_csv(Path(__file__).parent / 'Azadi2023_fig2B_fig_S3A.csv')
+    print(data)
+
+    # package into xarray
+    assembly = DataAssembly(data['corr_coeff'], coords={
+        'monkey': ('measurement', data['monkey']),
+        'site': ('measurement', data['site']),
+        'violin_part': ('measurement', data['violin_part']),
+        'summary_stats': ('measurement', data['summary_stats']),
+    }, dims=['measurement'])
+    assembly = assembly.unstack()
+    assembly['monkey_Ph_corr_median'] = assembly.where((assembly.monkey == 'Ph') & \
+                                                  (assembly.violin_part == 'center')).corr_coeff
+    assembly['monkey_Sp_corr_median'] = assembly.where((assembly.monkey == 'Sp') & \
+                                                  (assembly.violin_part == 'center')).corr_coeff
+    assembly = assembly.stack(condition=['monkey'])
+    assembly = DataAssembly(assembly)
+    return assembly
+
+
+def collect_psychometric_functions_illumination_power():
+    """ Azadi2023_fig2C_figS3BCD """
+    # data extracted with https://apps.automeris.io/wpd/ on 2023-08-18, points manually selected
+    data = pd.read_csv(Path(__file__).parent / 'Azadi2023_fig2C_figS3BCD.csv')
+    print(data)
+
+    # package into xarray
+    assembly = DataAssembly(data['performance_d_prime'], coords={
+        'monkey': ('measurement', data['monkey']),
+        'site': ('measurement', data['site']),
+        'image_id_color': ('measurement', data['image_id_color']),
+        'illumination_power_mW': ('measurement', data['illumination_power_mW']),
+        'summary_stats': ('measurement', data['summary_stats']),
+    }, dims=['measurement'])
+    assembly = assembly.unstack()
+    assembly['monkey_Ph'] = ('monkey', [monkey == 'Ph' for monkey in assembly['monkey']])
+    assembly['monkey_Sp'] = ('monkey', [monkey == 'Sp' for monkey in assembly['monkey']])
+    assembly = assembly.stack(condition=['monkey'])
+    assembly = DataAssembly(assembly)
+    return assembly
+
+
+def collect_psychometric_functions_image_visibility():
+    """ Azadi2023_fig3_figS3E """
+    # data extracted with https://apps.automeris.io/wpd/ on 2023-08-18, points manually selected
+    data = pd.read_csv(Path(__file__).parent / 'Azadi2023_fig3_figS3E.csv')
+    print(data)
+
+    # package into xarray
+    assembly = DataAssembly(data['performance_d_prime'], coords={
+        'monkey': ('measurement', data['monkey']),
+        'image_visibility_condition': ('measurement', data['image_visibility_condition']),
+    }, dims=['measurement'])
+    assembly = assembly.unstack()
+    assembly['monkey_Ph'] = ('monkey', [monkey == 'Ph' for monkey in assembly['monkey']])
+    assembly['monkey_Sp'] = ('monkey', [monkey == 'Sp' for monkey in assembly['monkey']])
+    assembly = assembly.stack(condition=['monkey'])
+    assembly = DataAssembly(assembly)
+    return assembly
